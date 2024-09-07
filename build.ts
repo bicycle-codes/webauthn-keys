afe4af4ddfe021b442af590ea29557069150ad8f
nichoth <nichoth@nichoth.com>
2024-09-07 09:53:04 -0700

    fix build

--- build.ts
+++ build.ts
@@ -1,26 +1,34 @@
-import fastGlob from 'fast-glob'
+import { globby } from 'globby'
 import esbuild from 'esbuild'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+const glob = await globby(['src/*.ts', '!src/*.test.ts'])
+
+// JS
 await esbuild.build({
-    entryPoints: await fastGlob('src/^*.test.ts'),
+    entryPoints: glob,
     keepNames: true,
     minify: false,
     target: 'es2022',
     sourcemap: true,
-    outdir: 'dist',
+    outdir: path.join(__dirname, 'dist'),
     platform: 'browser',
     format: 'esm',
     metafile: true,
     tsconfig: 'tsconfig.build.json',
 })
 
+// minified
 await esbuild.build({
-    entryPoints: await fastGlob('src/^*.test.ts'),
+    entryPoints: glob,
     keepNames: true,
     minify: true,
     target: 'es2022',
     sourcemap: true,
-    outdir: 'dist',
+    outdir: path.join(__dirname, 'dist'),
     outExtension: { '.js': '.min.js' },
     platform: 'browser',
     format: 'esm',
